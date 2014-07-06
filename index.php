b41af21f66c179215801765fa5a99dd6a7689400
gsakis <gkyrtsakas@gmail.com>
2014-07-06 12:58:31 -0400

    Chart is now separate from index
    Made charts available from app

--- index.php
+++ index.php
@@ -9,33 +9,9 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/starter-template.css" rel="stylesheet">
 
-    <!--Load the AJAX API-->
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script type="text/javascript">
     
-    // Load the Visualization API and the piechart package.
-    google.load('visualization', '1', {'packages':['corechart']});
-      
-    // Set a callback to run when the Google Visualization API is loaded.
-    google.setOnLoadCallback(drawChart);
-      
-    function drawChart() {
-      var jsonData = $.ajax({
-          url: "getData.php",
-          dataType:"json",
-          async: false
-          }).responseText;
-          
-      // Create our data table out of JSON data loaded from server.
-      var data = new google.visualization.DataTable(jsonData);
 
-      // Instantiate and draw our chart, passing in some options.
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-      chart.draw(data, {width: 1600, height: 600});
-    }
-
-    </script>
+    
   </head>
 
   <body>
@@ -74,7 +50,9 @@
       </div>
     </div>
 
-    <div id="chart_div"></div>
+    <div>
+      <?php include("chart.php"); ?>
+    </div>
 
 
 
